2c60ad3e82ae48ac888be81e1022377d95a1d27d
Franziska Adam <franziskacarolineadam@gmail.com>
2023-11-27 12:01:41 +0100

    Complete Task1 + Task2

--- components/ISSTracker/index.js
+++ components/ISSTracker/index.js
@@ -1,10 +1,17 @@
 import { useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
+import useSWR from "swr";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
 export default function ISSTracker() {
+  const { data, error, isLoading } = useSWR(URL);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  /*
   const [coords, setCoords] = useState({
     longitude: 0,
     latitude: 0,
@@ -31,14 +38,15 @@ export default function ISSTracker() {
       clearInterval(timer);
     };
   }, []);
+  */
 
   return (
     <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Map longitude={data.longitude} latitude={data.latitude} />
       <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
-        onRefresh={getISSCoords}
+        longitude={data.longitude}
+        latitude={data.latitude}
+        // onRefresh={getISSCoords}
       />
     </main>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <SWRConfig value={{ fetcher, refreshInterval: 5000 }}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </SWRConfig>
   );
 }
